5775fa6e403e0bc7e6b1bee1d1960f1f11b88543
rds1979 <expert.info79@gmail.com>
2022-12-04 22:30:38 +0300

    Lection 3 done

---
+++ lesson1/01_01_example.py
@@ -0,0 +1,10 @@
+a = b = 5
+
+print(f"id(a) = {id(a)}, id(b) = {id(b)}")
+print(f"a is b = {a is b}")
+
+lst1 = [1, 2, 3, 4, 5]
+lst2 = [1, 2, 3, 4, 5]
+
+print(f"id(lst1) = {id(lst1)}, id(lst2) = {id(lst2)}")
+print(f"lst1 is lst2 = {lst1 is lst2}")
